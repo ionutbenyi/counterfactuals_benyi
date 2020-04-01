96cc727b24dd766f0048e677beb2d473010b72c5
ionutbenyi <37183723+ionutbenyi@users.noreply.github.com>
2020-04-01 17:53:18 +0300

    smth better

--- modules/doc_scanner.py
+++ modules/doc_scanner.py
@@ -1,8 +1,12 @@
 import spacy
 import json
+import math
 
 
-trusted_sites = ['reuters', 'nytimes', 'bbc', 'ft.', '.economist.com', '.theguardian.', 'dailymail', 'time']
+trusted_sites = ['.reuters.', 'https://www.nytimes.com/', 
+'https://www.bbc.com/', 'https://www.ft.com/',
+'https://www.economist.com/', 'https://www.theguardian.com/', 
+'https://www.dailymail.co.uk/', 'https://time.com/']
 
 def check_trusted(site):
     for option_site in trusted_sites:
@@ -36,11 +40,11 @@ if __name__ == '__main__':
                 doc2 = nlp(article_text["content"])
                 if doc2.vector_norm:
                     similarity_value = doc1.similarity(doc2)
-                    if check_trusted(article_text["source"]) and similarity_value >= 0.95:
+                    if check_trusted(article_text["source"]) and similarity_value >= 0.924:
                         is_fact = True
                         good_sites_nr += 1
         
-                    if similarity_value >= 0.96:
+                    if math.floor(similarity_value) >= 0.96 and is_fact == False:
                         good_sites_nr += 1
             print("SIMILARITY = "+str(similarity_value)+" - "+str(article_text["source"]))
 
@@ -57,7 +61,7 @@ if __name__ == '__main__':
                 fake_truths += 1
                 count_facts_found -= 1    
 
-        elif good_sites_nr > (total_sites_nr/2) and is_fact == False and good_sites_nr > 0:
+        elif good_sites_nr >= math.ceil(total_sites_nr/2) and is_fact == False and good_sites_nr > 0:
             print(headline["sentence"]+" is FACT - "+str(headline["truth_flag"]))
             count_facts_found += 1
             if str(headline["truth_flag"]) == "0":
