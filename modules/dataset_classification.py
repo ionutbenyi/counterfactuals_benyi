f40fa21c6a22fa09f1e907191c9c4f1cfbeaef77
ionutbenyi <37183723+ionutbenyi@users.noreply.github.com>
2020-04-17 15:34:57 +0300

    with logs

--- modules/dataset_classification.py
+++ modules/dataset_classification.py
@@ -11,8 +11,8 @@ from modules.doc_scanner import DocScanner
 def detect_counterfactuals_from_sentence(sentence, keyword_selector, doc_gatherer, doc_scanner):
     split_sentence_json = keyword_selector.split_into_keywords(sentence)
     documents_json = doc_gatherer.gather_articles_for_sentence(sentence, split_sentence_json["search"])
-    is_fact = doc_scanner.scan_document(documents_json)
-    print(is_fact)
+    is_error = doc_scanner.check_error_case(documents_json)
+    return is_error
 
 if __name__ == '__main__':
 
@@ -26,11 +26,27 @@ if __name__ == '__main__':
     spacy_model = spacy.load("en_core_web_lg")
     doc_scanner = DocScanner(bert_model, spacy_model, debug_logs)
 
-    while(True):
-        sentence = input("Enter your sentence:\n")
-        if sentence == 'exit':
-            break
+    input_train_data=[]
+    with open('data/train_json.txt') as input_json_set:
+        input_train_data = json.load(input_json_set)
+    
+    correct_jsons = []
+    incorrect_jsons = []
+    count = 0
+
+    for i in range(1000):
+        print(count)
+        input_json = input_train_data[i]
+        error_flag = detect_counterfactuals_from_sentence(input_json["sentence"], keyword_selector, doc_gatherer, doc_scanner)
+        if error_flag: 
+            incorrect_jsons.append(input_json)
         else:
-            detect_counterfactuals_from_sentence(sentence, keyword_selector, doc_gatherer, doc_scanner)
+            correct_jsons.append(input_json)
+
+        count += 1
+    with open('data/corrected_set/train_json_errors.txt','w') as error_inputs_file:
+        json.dump(incorrect_jsons, error_inputs_file)
+    with open('data/corrected_set/train_json_correct.txt','w') as correct_inputs_file:
+        json.dump(correct_jsons, correct_inputs_file)
 
     
